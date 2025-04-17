d73268a870ebfd6e901b15067006bfcfa02a1361
eburke <eburke@mitre.org>
2025-04-17 08:53:32 -0400

    Rewrote the code to support Domain amd DomainDictionary and added domain field to TypeName and used that field to set the last namePackage string element to domain.toLowerCase. Added SourceContent to interact with persistent storage

---
+++ src/main/scala/draco/domain/SourceContent.scala
@@ -0,0 +1,30 @@
+package draco.domain
+
+import java.net.{URI, URL}
+import scala.io.{BufferedSource, Source}
+
+trait SourceContent {
+  val resourceClass: Class[_]
+  val resourceURL: URL
+  val resourcePath: String
+  val resourceURI: URI
+  val source: BufferedSource
+  val sourceLines: Seq[String]
+  val sourceString: String
+}
+
+object SourceContent {
+  def apply(
+             _resourcePath: String,
+             _resourceClass: Class[_]
+           ) : SourceContent = new SourceContent {
+    override val resourcePath: String = _resourcePath
+    override val resourceClass: Class[_] = _resourceClass
+    override val resourceURL: URL = resourceClass.getResource(resourcePath)
+    override val resourceURI: URI = resourceURL.toURI
+    override val source: BufferedSource = Source.fromFile(resourceURL.toURI)
+    override val sourceLines: Seq[String] = source.getLines.toSeq
+    override val sourceString: String = sourceLines.mkString
+    source.close()
+  }
+}
